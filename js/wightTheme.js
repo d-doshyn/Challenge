0d53b9b30401a0e0a93ecbf064c1efa25951f6a8
Sashlychok <93088489+Sashlychok@users.noreply.github.com>
2022-04-18 16:57:03 +0300

    комент для Миши

---
+++ js/wightTheme.js
@@ -0,0 +1,8 @@
+let WightButton = document.getElementById("themes");
+let body = document.querySelector("body");
+
+
+WightButton.onclick = function(){
+	body.style.backgroundColor = "#ffffff";
+	body.style.color = "black";
+}
