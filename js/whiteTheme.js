6363c5d5c0636a749af9bee993cc3c847b29aa4a
Sashlychok <93088489+Sashlychok@users.noreply.github.com>
2022-04-28 18:21:51 +0300

    white theme

--- js/whiteTheme.js
+++ js/whiteTheme.js
@@ -1,16 +1,30 @@
-let WhiteButton = document.getElementById("themes");
+let Icon = document.getElementById("theme-icon");
+let Button = document.getElementById("themes");
 let body = document.querySelector("body");
 let Model = document.querySelector(".model");
 let Logo = document.querySelector(".main-image");
 let Header = document.querySelector(".fixed-header");
 let HeaderLogo = document.querySelector(".image-title");
 
-WhiteButton.onclick = function () {
-	body.style.backgroundColor = "white";
-	body.style.color = "black";
-	Header.style.backgroundColor = "white";
-	Header.style.color = "black";
-	HeaderLogo.src = "img/logo_white.png";
-	Model.src = "img/white-woman.png";
-	Logo.src = "img/logo_white.png";
+function ToggleTheme() {
+	if (Icon.classList.contains("fa-sun")) {
+		HeaderLogo.src = "img/logo_white.png";
+		Model.src = "img/white-woman.png";
+		Logo.src = "img/logo_white.png";
+		Header.classList.add("white-bg");
+		body.classList.add("white-bg");
+		Icon.classList.remove("fa-sun");
+		Icon.classList.add("fa-moon");
+	}
+	else{
+		Header.classList.remove("white-bg"); 
+		body.classList.remove("white-bg");
+		HeaderLogo.src = "img/logo-main.png";
+		Model.src = "img/model-1.png";
+		Logo.src = "img/logo-main.png";
+		Icon.classList.add("fa-sun");
+		Icon.classList.remove("fa-moon");
+	}
 };
+
+Button.onclick = ToggleTheme ;
