726e59849c7435c2c9270e9e6d4825e60ee902bd
Sashlychok <93088489+Sashlychok@users.noreply.github.com>
2022-04-28 19:33:17 +0300

    fast pull

--- js/whiteTheme.js
+++ js/whiteTheme.js
@@ -5,16 +5,26 @@ let Model = document.querySelector(".model");
 let Logo = document.querySelector(".main-image");
 let Header = document.querySelector(".fixed-header");
 let HeaderLogo = document.querySelector(".image-title");
+let Search = document.querySelector(".search");
+let ManText = document.querySelector("#mens");
+let BoysText = document.querySelector("#boys");
+let WomanText = document.querySelector("#womans");
+let GirlsText = document.querySelector("#girls");
 
 function ToggleTheme() {
 	if (Icon.classList.contains("fa-sun")) {
-		HeaderLogo.src = "img/logo_white.png";
-		Model.src = "img/white-woman.png";
-		Logo.src = "img/logo_white.png";
+		HeaderLogo.src = "img/white-bg/logo_white.png";
+		Model.src = "img/white-bg/white-woman.png";
+		Logo.src = "img/white-bg/logo_white.png";
 		Header.classList.add("white-bg");
 		body.classList.add("white-bg");
 		Icon.classList.remove("fa-sun");
 		Icon.classList.add("fa-moon");
+		Search.classList.add(".white-bg");
+		ManText.classList.add(".black-text");
+		WomanText.classList.add(".black-text");
+		GirlsText.classList.add(".black-text");
+		BoysText.classList.add(".black-text");
 	}
 	else{
 		Header.classList.remove("white-bg"); 
@@ -24,6 +34,11 @@ function ToggleTheme() {
 		Logo.src = "img/logo-main.png";
 		Icon.classList.add("fa-sun");
 		Icon.classList.remove("fa-moon");
+		Search.classList.remove(".white-bg");
+		ManText.classList.remove(".black-text");
+		WomanText.classList.remove(".black-text");
+		GirlsText.classList.remove(".black-text");
+		BoysText.classList.remove(".black-text");
 	}
 };
 
