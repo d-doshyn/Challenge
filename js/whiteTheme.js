e8dd9923a3781b3807d895384540e8e618a976f1
Sashlychok <93088489+Sashlychok@users.noreply.github.com>
2022-05-15 20:20:25 +0300

    сырой код смены тем

--- js/whiteTheme.js
+++ js/whiteTheme.js
@@ -6,10 +6,8 @@ let Logo = document.querySelector(".main-image");
 let Header = document.querySelector(".fixed-header");
 let HeaderLogo = document.querySelector(".image-title");
 let Search = document.querySelector(".search");
-let ManText = document.querySelector("#mens");
-let BoysText = document.querySelector("#boys");
-let WomanText = document.querySelector("#womans");
-let GirlsText = document.querySelector("#girls");
+let Mark = document.querySelectorAll(".text-mark");
+
 
 function ToggleTheme() {
 	if (Icon.classList.contains("fa-sun")) {
@@ -20,26 +18,25 @@ function ToggleTheme() {
 		body.classList.add("white-bg");
 		Icon.classList.remove("fa-sun");
 		Icon.classList.add("fa-moon");
-		Search.classList.add(".white-bg");
-		ManText.classList.add(".black-text");
-		WomanText.classList.add(".black-text");
-		GirlsText.classList.add(".black-text");
-		BoysText.classList.add(".black-text");
+		Search.classList.add("white-bg");
+		for (var el of Mark) {
+			el.classList.add("black-text");
+		}
 	}
-	else{
-		Header.classList.remove("white-bg"); 
+	else {
+		Header.classList.remove("white-bg");
 		body.classList.remove("white-bg");
 		HeaderLogo.src = "img/logo-main.png";
 		Model.src = "img/model-1.png";
 		Logo.src = "img/logo-main.png";
 		Icon.classList.add("fa-sun");
 		Icon.classList.remove("fa-moon");
-		Search.classList.remove(".white-bg");
-		ManText.classList.remove(".black-text");
-		WomanText.classList.remove(".black-text");
-		GirlsText.classList.remove(".black-text");
-		BoysText.classList.remove(".black-text");
+		Search.classList.remove("white-bg");
+		for (var el of Mark) {
+			el.classList.remove("black-text");
+		}
 	}
 };
 
-Button.onclick = ToggleTheme ;
+
+Button.onclick = ToggleTheme;
